8ef4285f5d71367cc1e63dc18041e66c976c994a
codeX3616 <aakkuu3616@gmail.com>
2023-04-16 15:42:44 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUp from "./SignUp";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default App;
